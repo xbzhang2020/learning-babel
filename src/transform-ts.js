bb44c541faa0fde15b1f0d6c68d8e5e0f1c46182
xbzhang2020 <xbzhang2020@icloud.com>
2023-04-30 08:33:29 +0800

    feat: ts

--- src/transform-ts.js
+++ src/transform-ts.js
@@ -1,16 +1,20 @@
-import { transformSync } from "@babel/core";
 import { readFileSync } from "node:fs";
 import { join, dirname } from "node:path";
 import { fileURLToPath } from "url";
+import { transformSync } from "@babel/core";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const fileName = join(__dirname, "./examples/index.ts");
 
 const code = readFileSync(fileName, "utf-8");
 
-const res = transformSync(code, {
-  presets: ["@babel/preset-typescript"],
-  filename: "script.ts",
-});
+// const res = transformSync(code, {
+//   presets: ["@babel/preset-typescript"],
+//   filename: "script.ts",
+// });
 
-console.log(res.code);
+const res = transformSync(code, {
+  plugins: ["@babel/plugin-transform-typescript"],
+});
+console.log(res);
+// console.log(res.code);
