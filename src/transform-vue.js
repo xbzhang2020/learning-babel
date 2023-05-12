6cb52f1e594343abd073098c57808c5095a1f8ed
xbzhang2020 <xbzhang2020@icloud.com>
2023-05-12 08:32:09 +0800

    feat: 装饰器语法

--- src/transform-vue.js
+++ src/transform-vue.js
@@ -8,7 +8,7 @@ import { transformVue } from './plugins/transform-vue.js'
 
 // 1. 读取 vue 文件
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const fileName = join(__dirname, './examples/vue/option-base.vue')
+const fileName = join(__dirname, './examples/vue/class-base.vue')
 const code = readFileSync(fileName, 'utf-8')
 
 // 2. 解析 script 部分
@@ -17,14 +17,14 @@ const script = sfc.script
 // console.log(sfc.script.content)
 
 // 3. 使用 babel 处理 script 部分
-// const ast = parse(scriptContent, { sourceType: "module" });
+// const ast = parse(script, { sourceType: "module" });
 // console.log(ast.program.body);
 const res = transformSync(script.content, {
   sourceType: 'module',
-  plugins: [transformVue],
+  plugins: [['@babel/plugin-proposal-decorators', { version: '2023-01' }]],
 })
-const replaceScript = res.code
+console.log(res.code)
 
-// 4. 输出结果
-const content = `${code.slice(0, script.start)}\n${replaceScript}\n${code.slice(script.end)}`
-console.log(content)
+// // 4. 输出结果
+// const content = `${code.slice(0, script.start)}\n${replaceScript}\n${code.slice(script.end)}`
+// console.log(content)
